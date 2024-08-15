2f5f89d6eb18e7bb06b8af1519d1cf4cfe247e2f
Jorge Miranda Zuniga <jorgaismiranda@gmail.com>
2024-08-14 18:50:21 -0600

    Add: Modificacion de estilos en JavaScript

--- Manipulacion_del_DOM/Manipulando_Elementos_del_DOM/main.js
+++ Manipulacion_del_DOM/Manipulando_Elementos_del_DOM/main.js
@@ -10,10 +10,10 @@ valor inicial que le dimos en nuestro documento HTML, dado que el
 inicial solo es un atributo con el que se inicializa el objeto que
 vamos a manipular con JavaScript
 */
-console.log(input.value)  // Aqui nos imprime el valor inicial, declarado
+//console.log(input.value)  // Aqui nos imprime el valor inicial, declarado
 // en los atributos del input
-input.value = 'Nombre completo' 
-console.log(input.value)  // Ya que  modificamos el valor de la propieda, 
+//input.value = 'Nombre completo' 
+//console.log(input.value)  // Ya que  modificamos el valor de la propieda, 
 // directamente con JavaScript ya no mostrara 'Nombre', mostrar 'Nombre completo'
 
 //! 2. Modificacion de texto en HTML
@@ -23,3 +23,22 @@ title.textContent = 'Texto cambiado desde JavaScript' // Este y el metodo de aba
 // si van a cambiar el texto que tiene esta etiqueta, dado que no es un
 // atributo de la etiqueta
 title.innerText = 'Otro titulo cambiado'
+
+//! 3. Modificacion de estilos
+// 3.1 Propiedad .style: Agrega estilos en linea en la etiqueta del elemento.
+const title2 = document.querySelector('h1')
+title.style.color = 'red'
+const menu = document.querySelector('.menu')
+menu.style.backgroundColor = 'pink'
+menu.style.fontSize = '25px'
+
+// 3.2 className: Nos ayuda a acceder a la clase ya existente de un elemento,
+// y una vez que accede reescribe la clase del elemento por una nueva
+menu.className = 'main-menu'
+
+// 3.2 classList: Nos permite acceder a las clases de un elemento
+// y agregar nuevas clases sin reescribir las clases ya existentes
+const button = document.querySelector('button')
+button.addEventListener('click', () => {
+  menu.classList.toggle('invisible')  // con este metodo vamos a alternar la presencia de una clase
+})
