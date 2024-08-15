db22214299240c000775405bd6b2087b943ee568
Jorge Miranda Zuniga <jorgaismiranda@gmail.com>
2024-08-14 18:02:52 -0600

    Add: Cambio de los atributos de un elemento HTML

---
+++ Manipulacion_del_DOM/Manipulando_Elementos_del_DOM/main.js
@@ -0,0 +1,16 @@
+const input = document.querySelector('#input-id')
+console.log(input)  // nos devolvera el elemento de input
+console.dir(input)  // nos devolvera las propiedades del objeto input
+
+/*
+Al hacer eso, el valor de input va a cambiar, pero solo se cambio en 
+la propiedad del objeto ya cargado en el DOM, esto no va a alterar el 
+valor inicial que le dimos en nuestro documento HTML, dado que el 
+inicial solo es un atributo con el que se inicializa el objeto que
+vamos a manipular con JavaScript
+*/
+console.log(input.value)  // Aqui nos imprime el valor inicial, declarado
+// en los atributos del input
+input.value = 'Nombre completo' 
+console.log(input.value)  // Ya que  modificamos el valor de la propieda, 
+// directamente con JavaScript ya no mostrara 'Nombre', mostrar 'Nombre completo'
