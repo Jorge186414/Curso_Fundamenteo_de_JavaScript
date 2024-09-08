3eb2bff01f9dc705bdec802250b4abf3a3e8c793
Jorge Miranda Zuniga <jorgaismiranda@gmail.com>
2024-09-08 00:05:31 -0600

    ADD: Forma de agregar eventos y eliminarlos con ayuda de callback

--- Manipulacion_del_DOM/Manipulando_Eventos_del_DOM/main.js
+++ Manipulacion_del_DOM/Manipulando_Eventos_del_DOM/main.js
@@ -8,3 +8,21 @@ container.addEventListener('mouseover', () => {
 container.addEventListener('mouseout', () => {
   container.style.backgroundColor = 'red'
 })
+
+//button.addEventListener('click', () => {
+//  alert('Button clicked')
+//})
+
+// Como retirar eventos una vez ejecutados
+// Creamos la funcion que se va a ejecutar al accionar un evento
+const buttonClickCallback = () => {
+  alert('Button Clicked')
+}
+// agregar esa funcion como evento a un elemento
+button.addEventListener('click', buttonClickCallback)
+
+// creamos un time con setTimeout, que es una web api que nos da el navegado
+setTimeout(() => {
+  button.removeEventListener('click', buttonClickCallback)  // primer parametro indicamos que vento queremos eliminar
+  // el segundo parametro es el que pasara el callback para saber que evento se va a eliminar
+}, 2000)  // indica el tiempo en milisigundos que tienen que pasar para que elimine el evento
