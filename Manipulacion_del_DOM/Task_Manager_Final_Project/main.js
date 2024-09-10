26fc547c46bd40c2649f6eec50038d19d796a190
Jorge186414 <jorgaismiranda@gmail.com>
2024-09-10 02:05:34 -0600

    ADD: Funcion para eliminar elementos del localStorage y explicacion de funciones sin explicacion

--- Manipulacion_del_DOM/Task_Manager_Final_Project/main.js
+++ Manipulacion_del_DOM/Task_Manager_Final_Project/main.js
@@ -7,6 +7,7 @@ loadTask()
 
 //! Agregar evento al formulario para que se manden los datos de este
 taskForm.addEventListener('submit', (event) => {
+  // Evitamos el comportamiento normal del formulario
   event.preventDefault()
 
   // Obtenemos el input para el task y recuperamos el valor de esta
@@ -50,9 +51,16 @@ function createButton(text, className) {
 
 //! Uso de delegacion de evento para los botones de editar y borrar
 taskList.addEventListener('click', (event) => {
+  // En base al eventObject validamos la clase que este tiene
+  // al momento de que el padre(taskList) delega un evento al elemento
+  // hijo(span) que presionamos se decide la validacion
   if (event.target.classList.contains('delete-btn')) {
+    // Si la clase es delete-btn se ejecuta la funcion deleteTask ayudandonos 
+    // del target del evento
     deleteTask(event.target.parentElement)
   } else if (event.target.classList.contains('edit-btn')) {
+    // Si la clase es edit-btn se ejecuta la funcion editTask igualmente
+    // ayudandonos del target del evento
     editTask(event.target.parentElement)
   }
 })
@@ -60,15 +68,23 @@ taskList.addEventListener('click', (event) => {
 //! Funcion para borrar un elemento, lo que equivale a borrar una tarea
 const deleteTask = taskItem => {
   if (confirm('Segur@ que deseas borrar la tarea?')) {
+    // Mandamos a llamar a la funcion removeFromLocaStorage, y este parametro
+    // corresponde al nombre de la tarea que hay en el li donde damos clic 
+    removeFromLocaStorage(taskItem.firstChild.textContent)
     taskItem.remove()
   }
 }
 
 //! Funcion para editar el elemento
 const editTask = taskItem => {
+  // Se muestra un promp que tendra la tarea a editar, pasada por parametro al 
+  // mandar llamar la funcion 
   const newTask = prompt('Edita la tarea: ', taskItem.firstChild.textContent)
+  // si borramos el texto no va a ejecutar nada, en casi contrario
   if (newTask !== null) {
+    // cambiamos el texto del primer hijo del item por el nuevo ingresado en el promp
     taskItem.firstChild.textContent = newTask
+    // Actualizamos lo almacenado en el localStorage
     updateLocalStorage()
   }
 }
@@ -77,7 +93,7 @@ const editTask = taskItem => {
 const storeTaskInLocalStorage = task => {
   // Creamos una variable para las tareas, las parsearemos a un archivo JSON
   // primero obetenemos lo que hay en el el localStorage.
-  // Y si no hay nada lo almacenamos en un array vacio
+  // Y si no hay nada almacenamos en un array vacio
   const tasks = JSON.parse(localStorage.getItem('tasks') || '[]')
   // Mandamos la nueva tarea al localStorage
   tasks.push(task)
@@ -85,7 +101,11 @@ const storeTaskInLocalStorage = task => {
 }
 
 function loadTask() {
+  // Obtenemos lo que tenemos almacenado en el localStorage, si no hay 
+  // algo almacenado se genera un array vacion
   const tasks = JSON.parse(localStorage.getItem('tasks') || '[]')
+  // Para cada tarea del localStorage vamos a crear un item dentro 
+  // de nuestra lista de tareas
   tasks.forEach(task => {
     taskList.appendChild(createTaskElement(task))
   })
@@ -97,7 +117,15 @@ const updateLocalStorage = () => {
   // li en nuestra liesta de tareas, una vez obtenidos vamos a obtener el texto del
   // primer elemento del li, y todo esto lo vamos a pasar a un array con Array.from()
   const tasks = Array.from(taskList.querySelectorAll('li')).map((li) => li.firstChild.textContent)
-  
-  // Empujar esto al localStorage para que almacene el cambio
+  // Empujamos esto al localStorage para que almacene el cambio
   localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+const removeFromLocaStorage = (taskContent) => {
+  // Obtenemos las tareas del localStorage, y si no hay nada almacenamos un array vacion
+  const tasks = JSON.parse(localStorage.getItem('tasks') || '[]')
+  // Filtramos lo que hay en la lista de tareas en base lo que se pasa como parametro
+  const updateTasks = tasks.filter(task => task !== taskContent)
+  // Seteamos la actualizacion en el localStorage
+  localStorage.setItem('tasks', JSON.stringify(updateTasks))
 }
